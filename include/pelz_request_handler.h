f28acbc0833be5830f5cc679d977c8c14ad7562b
bit-bender <53864796+bit-bender@users.noreply.github.com>
2024-05-17 21:30:08 -0400

    added PELZ_MSG ASN1 baseline

--- include/pelz_request_handler.h
+++ include/pelz_request_handler.h
@@ -9,7 +9,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <openssl/asn1.h>
+#include <openssl/asn1t.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/x509.h>
+#include <openssl/cms.h>
+#include <openssl/evp.h>
+
 #include "charbuf.h"
+
+#define PELZ_UNSPECIFIED_MSG_TYPE -1
+#define PELZ_REQUEST_MSG_TYPE 0
+#define PELZ_RESPONSE_MSG_TYPE 1
+
+#define PELZ_UNSPECIFIED_REQ_TYPE 0
+#define PELZ_AES_KEY_WRAP_REQ_TYPE 1
+#define PELZ_AES_KEY_UNWRAP_REQ_TYPE 2
 
 //The maxim key length
 #define MAX_KEY_LEN 1024

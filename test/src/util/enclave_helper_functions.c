ca638fe4ead8e69c510c3b1b2125c405e2dc9ddf
bit-bender <53864796+bit-bender@users.noreply.github.com>
2024-05-28 19:18:45 -0400

    added create_signed_data_msg() test

--- test/src/util/enclave_helper_functions.c
+++ test/src/util/enclave_helper_functions.c
@@ -2,20 +2,29 @@
  * enclave_helper_functions.c
  */
 
+#include "enclave_helper_functions.h"
+
 #include "common_table.h"
 #include "charbuf.h"
 
 #include "sgx_trts.h"
 #include "test_enclave_t.h"
 #include "cipher/pelz_cipher.h"
+#include "pelz_messaging.h"
 
-TableResponseStatus test_table_lookup(TableType type, charbuf id, size_t *index)
+TableResponseStatus test_table_lookup(TableType type,
+                                      charbuf id,
+                                      size_t *index)
 {
   return (table_lookup(type, id, index));
 }
 
-int test_aes_keywrap_3394nopad_encrypt(size_t key_len, unsigned char *key, size_t inData_len, unsigned char *inData,
-  size_t * outData_len, unsigned char **outData)
+int test_aes_keywrap_3394nopad_encrypt(size_t key_len,
+                                       unsigned char *key,
+                                       size_t inData_len,
+                                       unsigned char *inData,
+                                       size_t *outData_len,
+                                       unsigned char **outData)
 {
   int ret = -1;
   unsigned char *output = NULL;
@@ -39,8 +48,12 @@ int test_aes_keywrap_3394nopad_encrypt(size_t key_len, unsigned char *key, size_
   return (ret);
 }
 
-int test_aes_keywrap_3394nopad_decrypt(size_t key_len, unsigned char *key, size_t inData_len, unsigned char *inData,
-  size_t * outData_len, unsigned char **outData)
+int test_aes_keywrap_3394nopad_decrypt(size_t key_len,
+                                       unsigned char *key,
+                                       size_t inData_len,
+                                       unsigned char *inData,
+                                       size_t *outData_len,
+                                       unsigned char **outData)
 {
   int ret = -1;
   unsigned char *output = NULL;
@@ -62,4 +75,67 @@ int test_aes_keywrap_3394nopad_decrypt(size_t key_len, unsigned char *key, size_
     memcpy(*outData, output, *outData_len);
   }
   return (ret);
+}
+
+int test_create_signed_data_msg_helper(size_t test_data_in_len,
+                                       uint8_t *test_data_in,
+                                       size_t test_der_sign_cert_len,
+                                       const uint8_t *test_der_sign_cert,
+                                       size_t test_der_sign_priv_len,
+                                       const uint8_t *test_der_sign_priv)
+{
+  X509 *test_cert = NULL;
+  if ((test_der_sign_cert != NULL) && (test_der_sign_cert_len != 0))
+  {
+    d2i_X509(&test_cert, &test_der_sign_cert, (int) test_der_sign_cert_len);
+    if (test_cert == NULL)
+    {
+      return SIGN_TEST_INVALID_DER_CERT;
+    }
+  }
+
+  EVP_PKEY *test_priv = NULL;
+  if ((test_der_sign_priv != NULL) && (test_der_sign_priv_len != 0))
+  {
+    d2i_PrivateKey(EVP_PKEY_EC,
+                   &test_priv,
+                   &test_der_sign_priv,
+                   (int) test_der_sign_priv_len);
+    if (test_priv == NULL)
+    {
+      return SIGN_TEST_INVALID_DER_PRIV;
+    }
+  }
+
+  CMS_ContentInfo *signed_msg = NULL;
+  signed_msg = create_signed_data_msg(test_data_in,
+                                      (int) test_data_in_len,
+                                      test_cert,
+                                      test_priv);
+
+  if (signed_msg == NULL)
+  {
+    return SIGN_TEST_FAILURE;
+  }
+
+  if (OBJ_obj2nid(CMS_get0_type(signed_msg)) != NID_pkcs7_signed)
+  {
+    return SIGN_TEST_INVALID_RESULT;
+  }
+
+  ASN1_OCTET_STRING *signed_content = NULL;
+  signed_content = *(CMS_get0_content(signed_msg));
+  if (signed_content == NULL)
+  {
+    return SIGN_TEST_PARSE_ERROR;
+  }
+  const unsigned char * signed_data = NULL;
+  signed_data = ASN1_STRING_get0_data((const ASN1_STRING *) signed_content);
+  if (memcmp(test_data_in, signed_data, test_data_in_len) != 0)
+  {
+    return SIGN_TEST_INVALID_RESULT;
+  }
+
+  return SIGN_TEST_SUCCESS;
+
 }
--- test/src/util/pelz_messaging_test_suite.c
+++ test/src/util/pelz_messaging_test_suite.c
@@ -7,6 +7,8 @@
 #include <CUnit/CUnit.h>
 
 #include "pelz_messaging_test_suite.h"
+#include "enclave_helper_functions.h"
+
 #include <pelz_log.h>
 
 #include "sgx_urts.h"
@@ -34,6 +36,136 @@ int pelz_messaging_suite_add_tests(CU_pSuite suite)
 void test_create_signed_data_msg(void)
 {
   pelz_log(LOG_DEBUG, "Start create_signed_data_msg() functionality test");
+
+  int result = 0;
+
+  char *test_data = "Hello, pelz!";
+  size_t test_data_len = (size_t) strlen(test_data);
+
+  BIO *test_cert_bio = BIO_new_file("test/data/node_pub.pem", "r");
+  if (test_cert_bio == NULL)
+  {
+    CU_FAIL("error creating BIO for reading test cert from file");
+  }
+  X509 *test_cert = PEM_read_bio_X509(test_cert_bio, NULL, 0, NULL);
+  if (test_cert == NULL)
+  {
+    CU_FAIL("error creating test X509 certificate");
+  }
+  BIO_free(test_cert_bio);
+  int test_der_cert_len = -1;
+  uint8_t *test_der_cert = NULL;
+  test_der_cert_len = i2d_X509(test_cert, &test_der_cert);
+  if ((test_der_cert == NULL) || (test_der_cert_len <= 0))
+  {
+    CU_FAIL("error creating DER-formatted test certificate");
+  }
+
+  BIO * test_key_bio = BIO_new_file("test/data/node_priv.pem", "r");
+  if (test_key_bio == NULL)
+  {
+    CU_FAIL("error creating BIO for reading test key from file");
+  }
+  EVP_PKEY * test_key = PEM_read_bio_PrivateKey(test_key_bio, NULL, 0, NULL);
+  if (test_key == NULL)
+  {
+    CU_FAIL("error creating test EC private key for signing");
+  }
+  BIO_free(test_key_bio);
+  size_t test_der_key_len = 0;
+  uint8_t *test_der_key = NULL;
+  test_der_key_len = (size_t) i2d_PrivateKey(test_key, &test_der_key);
+  if ((test_der_key == NULL) || (test_der_key_len <= 0))
+      
+  {
+    CU_FAIL("error creating DER-formatted test EC private key");
+  }
+
+  BIO * mismatch_key_bio = BIO_new_file("test/data/worker_priv.pem", "r");
+  if (mismatch_key_bio == NULL)
+  {
+    CU_FAIL("error creating BIO for reading test key from file");
+  }
+  EVP_PKEY * mismatch_key = PEM_read_bio_PrivateKey(mismatch_key_bio, NULL, 0, NULL);
+  if (mismatch_key == NULL)
+  {
+    CU_FAIL("error creating test EC private key for signing");
+  }
+  BIO_free(mismatch_key_bio);
+  size_t mismatch_der_key_len = 0;
+  uint8_t *mismatch_der_key = NULL;
+  mismatch_der_key_len = (size_t) i2d_PrivateKey(mismatch_key, &mismatch_der_key);
+  if ((mismatch_der_key == NULL) || (mismatch_der_key_len <= 0))
+      
+  {
+    CU_FAIL("error creating DER-formatted test EC private key");
+  }
+
+  // valid test case should pass
+  test_create_signed_data_msg_helper(eid,
+                                     &result,
+                                     test_data_len,
+                                     (uint8_t *) test_data,
+                                     (size_t) test_der_cert_len,
+                                     test_der_cert,
+                                     test_der_key_len,
+                                     test_der_key);
+  CU_ASSERT(result == SIGN_TEST_SUCCESS);
+
+  // NULL input data  pointer should fail
+  test_create_signed_data_msg_helper(eid,
+                                     &result,
+                                     test_data_len,
+                                     NULL,
+                                     (size_t) test_der_cert_len,
+                                     test_der_cert,
+                                     test_der_key_len,
+                                     test_der_key);
+  CU_ASSERT(result == SIGN_TEST_FAILURE);
+  
+  // Empty input data buffer should fail
+  test_create_signed_data_msg_helper(eid,
+                                     &result,
+                                     0,
+                                     (uint8_t *) test_data,
+                                     (size_t) test_der_cert_len,
+                                     test_der_cert,
+                                     test_der_key_len,
+                                     test_der_key);
+  CU_ASSERT(result == SIGN_TEST_FAILURE);
+  
+  // NULL cert should fail
+  test_create_signed_data_msg_helper(eid,
+                                     &result,
+                                     test_data_len,
+                                     (uint8_t *) test_data,
+                                     (size_t) test_der_cert_len,
+                                     NULL,
+                                     test_der_key_len,
+                                     test_der_key);
+  CU_ASSERT(result == SIGN_TEST_FAILURE);
+
+  // NULL private signing key should fail
+  test_create_signed_data_msg_helper(eid,
+                                     &result,
+                                     test_data_len,
+                                     (uint8_t *) test_data,
+                                     (size_t) test_der_cert_len,
+                                     test_der_cert,
+                                     test_der_key_len,
+                                     NULL);
+  CU_ASSERT(result == SIGN_TEST_FAILURE);
+
+  // Mismatched key/cert should fail
+  test_create_signed_data_msg_helper(eid,
+                                     &result,
+                                     test_data_len,
+                                     (uint8_t *) test_data,
+                                     (size_t) test_der_cert_len,
+                                     test_der_cert,
+                                     mismatch_der_key_len,
+                                     mismatch_der_key);
+  CU_ASSERT(result == SIGN_TEST_FAILURE);
 }
 
 void test_verify_signature(void)
